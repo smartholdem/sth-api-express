b4b70dfb112fdda7533f028ed372b4e6347da6ac
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-08-20 17:38:25 +0300

    getAddress

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,70 @@ const axios = require('axios');
 const jsonFile = require('jsonfile');
 const config = jsonFile.readFileSync('./config.json');
 
+async function getAddress(address) {
+    let result = null;
+    try {
+        const data = (await axios.get(config.node + '/api/wallets/' + address)).data.data;
+        let attributes = data.attributes;
+        if (attributes['delegate']) {
+            attributes['voteBalance'] = (attributes['voteBalance'] / 1e8).toFixed(8);
+            attributes['forgedFees'] = (attributes['forgedFees'] / 1e8).toFixed(8);
+            attributes['forgedRewards'] = (attributes['forgedRewards'] / 1e8).toFixed(8);
+            if (attributes['delegate']['lastBlock']) {
+                let lastBlock = attributes['delegate']['lastBlock'];
+                lastBlock['totalAmount'] = (lastBlock['totalAmount'] / 1e8).toFixed(8);
+                lastBlock['totalFee'] = (lastBlock['totalFee'] / 1e8).toFixed(8);
+                lastBlock['reward'] = (lastBlock['reward'] / 1e8).toFixed(8);
+                attributes['delegate']['lastBlock'] = lastBlock;
+            }
+        }
+
+        result = {
+            address: data.address,
+            publicKey: data.publicKey || false,
+            balance: (data.balance / 1e8).toFixed(8),
+            nonce: data.nonce,
+            attributes: attributes,
+
+        }
+    } catch (e) {
+
+    }
+    return result;
+}
+
+
+async function getSupply() {
+    let result = null;
+    let burned = null;
+
+
+    try {
+        const data = (await axios.get(config.node + '/api/blockchain')).data.data;
+
+        result = {
+            chainId: data.block.id,
+            height: data.block.height,
+            supply: (data.supply / 1e8).toFixed(8),
+
+
+        }
+
+    } catch (e) {
+
+    }
+    return result;
+}
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SmartHoldem API Wrapper' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'SmartHoldem API Wrapper'});
 });
 
-router.get('/blockchain', async function(req, res, next) {
+router.get('/blockchain', async function (req, res, next) {
 
-  res.json(true)
+    res.json(true)
 });
 
 module.exports = router;
