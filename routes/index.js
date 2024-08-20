9c9454225f19731ca094a0b39f13fba245d99f27
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-08-20 17:20:03 +0300

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const jsonFile = require('jsonfile');
+const config = jsonFile.readFileSync('./config.json');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SmartHoldem API Wrapper' });
+});
+
+router.get('/blockchain', async function(req, res, next) {
+
+  res.json(true)
 });
 
 module.exports = router;
